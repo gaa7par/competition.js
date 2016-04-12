84b4085466e22f314dbf387a640875dc2c08f2f0
gaa7par <gaa7par@gmail.com>
2016-04-12 23:26:21 +0200

    fizFooBar.js refactored

--- fizFooBar.js
+++ fizFooBar.js
@@ -1,21 +1,9 @@
 "use strict";
 
 function fizFooBar(number, array) {
-    let pattern = {};
     let output = '';
-    
     for (let pair of array)
-        pattern[pair[1]] = pair[0];
-
-    for (let pair in pattern)
-        if (number % pattern[pair] == 0)
-            output += pair;
-    
+        if (number % pair[0] == 0)
+            output += pair[1];
     return output;
 }
-
-// test
-
-const number = 30;
-const array = [[2, "Two"], [3, "Three"], [5, "Five"]];
-console.log(fizFooBar(number, array));
