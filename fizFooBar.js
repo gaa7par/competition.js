c28a27e2840e7b3dd2ffb3ccca4df5ac09778c09
gaa7par <gaa7par@gmail.com>
2016-04-12 22:46:29 +0200

    FizFooBar.js solved

--- fizFooBar.js
+++ fizFooBar.js
@@ -2,15 +2,20 @@
 
 function fizFooBar(number, array) {
     let pattern = {};
+    let output = '';
     
-    for (let pair of array) {
-        pattern[pair[0]] = pair[1];
-    }
-    console.log(pattern);
+    for (let pair of array)
+        pattern[pair[1]] = pair[0];
+
+    for (let pair in pattern)
+        if (number % pattern[pair] == 0)
+            output += pair;
+    
+    return output;
 }
 
 // test
 
-const number = 20;
+const number = 30;
 const array = [[2, "Two"], [3, "Three"], [5, "Five"]];
 console.log(fizFooBar(number, array));
